2ba8d2952131182f07551991444a84f4d7fd400d
sufangru <sufangru@baibu.la>
2020-06-07 20:54:13 +0800

    init: 初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="container">
+    <div v-if="loading">loading</div>
+    <!-- <div class="appContainer" v-html="content">content</div> -->
+    <!-- 子应用盒子 -->
+    <div id="root-view" class="app-view-box" v-html="content"></div>
+    <!-- <router-view /> -->
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: 'App',
+  props: {
+    loading: Boolean,
+    content: String,
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,94 @@
+import { registerMicroApps, setDefaultMountApp, start } from 'qiankun';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+let app = null;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+/**
+ * 渲染函数
+ * appContent 子应用html内容
+ * loading 子应用加载效果，可选
+ *
+ * @param {*} [{ appContent, loading }={}]
+ * @returns
+ */
+function render({ appContent, loading } = {}) {
+  if (!app) {
+    app = new Vue({
+      el: '#app',
+      router,
+      store,
+      data() {
+        return {
+          content: appContent,
+          loading,
+        };
+      },
+      render(h) {
+        return h(App, {
+          props: {
+            content: this.content,
+            loading: this.loading,
+          },
+        });
+      },
+    });
+  } else {
+    app.content = appContent;
+    app.loading = loading;
+  }
+}
+
+/**
+ * 路由监听
+ *
+ * @param {*} routerPrefix 前缀
+ * @returns
+ */
+function genActiveRule(routerPrefix) {
+  return (location) => location.pathname.startsWith(routerPrefix);
+}
+
+
+function initApp() {
+  render({ appContent: '', loading: true });
+}
+
+initApp();
+
+// 传入子应用的数据
+const msg = {
+  data: {
+    auth: false,
+  },
+  fns: [
+    {
+      name: '_LOGIN',
+      LOGIN(data) {
+        console.log(`父应用返回信息${data}`);
+      },
+    },
+  ],
+};
+
+// 注册子应用
+registerMicroApps(
+  [
+    {
+      name: 'vueApp',
+      entry: '//localhost:7101',
+      render,
+      activeRule: genActiveRule('/vue'),
+      props: msg,
+    },
+  ],
+);
+
+// 设置默认子应用,与 genActiveRule中的参数保持一致
+setDefaultMountApp('/vue');
+
+// 启动
+start();
